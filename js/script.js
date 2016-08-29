d9948eac3e20eee0335fe6aec3b2c698ea991857
Akramov Sardor <akramovss@gmail.com>
2016-08-29 18:39:35 +0300

    После 7 раздела. После правок

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
-/* Попап */
-
 var bfind = document.querySelector(".btn-find");
 var popup = document.querySelector(".index-form");
-var arrive = popup.querySelector("[name=arrive]");
+var arrive = popup.querySelector(".js-arrive");
 var form = popup.querySelector(".index-search-form");
-var leave = popup.querySelector("[name=leave]");
+var leave = popup.querySelector(".js-leave");
+var jsfree = document.querySelector(".no-js");
 
-
+jsfree.classList.remove("no-js");
 
 bfind.addEventListener("click", function(event) {
   event.preventDefault();
-  popup.classList.toggle("index-form-show");
+  popup.classList.toggle("js-form-show");
   arrive.focus();
 });
 
 form.addEventListener("submit", function(event) {
   if (!arrive.value || !leave.value) {
     event.preventDefault();
-    popup.classList.remove("modal-error");
+    popup.classList.remove("js-modal-error");
     popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("modal-error");
+    popup.classList.add("js-modal-error");
   }
 });
 
 window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
-    if (popup.classList.contains("index-form-show")) {
-      popup.classList.remove("index-form-show");
+    if (popup.classList.contains("js-form-show")) {
+      popup.classList.remove("js-form-show");
     }
   }
 });
+
